9d55cbb15e55990dbb75ee24567198a93ac4bb07
Michael Granger <ged@FaerieMUD.org>
2019-09-02 13:53:12 -0700

    Add a bundle file

---
+++ gems.rb
@@ -0,0 +1,17 @@
+source "https://rubygems.org/"
+
+gem 'concurrent-ruby', '~> 1.1.5'
+gem 'concurrent-ruby-ext', '~> 1.1.5'
+gem 'loggability', '~> 0.11'
+gem 'configurability', '~> 3.3'
+gem 'pluggability', '~> 0.6'
+gem 'msgpack', '~> 1.3'
+
+group :development do
+	gem 'timecop', '~> 0.9'
+	gem 'hoe-deveiate', '~> 0.3'
+	gem 'simplecov', '~> 0.7'
+	gem 'rdoc-generator-fivefish', '~> 0.1'
+end
+
+
